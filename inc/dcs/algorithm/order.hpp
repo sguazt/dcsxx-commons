d84b83d6cfdc3d33645ec47ad7836cf7eed95260
sguazt <marco.guazzone@gmail.com>
2012-10-01 10:24:51 +0200

    (change|minor) License update.

--- inc/dcs/algorithm/order.hpp
+++ inc/dcs/algorithm/order.hpp
@@ -4,26 +4,31 @@
  * \brief Computed the permutation of indices which makes the given range
  *  ordered.
  *
- * Copyright (C) 2009-2012  Distributed Computing System (DCS) Group,
- *                          Computer Science Institute,
- *                          Department of Science and Technological Innovation,
- *                          University of Piemonte Orientale,
- *                          Alessandria (Italy).
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
  *
- * This program is free software: you can redistribute it and/or modify
+ * <hr/>
+ *
+ * Copyright (C) 2009-2012  Marco Guazzone
+ *                          [Distributed Computing System (DCS) Group,
+ *                           Computer Science Institute,
+ *                           Department of Science and Technological Innovation,
+ *                           University of Piemonte Orientale,
+ *                           Alessandria (Italy)]
+ *
+ * This file is part of dcsxx-commons.
+ *
+ * dcsxx-commons is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * dcsxx-commons is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <http://www.gnu.org/licenses/>.
- *
- * \author Marco Guazzone (marco.guazzone@gmail.com)
+ * along with dcsxx-commons.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #ifndef DCS_ALGORITHM_ORDER_HPP
