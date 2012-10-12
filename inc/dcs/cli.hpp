2791dc995cc21cf61041f3dc43a125b40259ab78
sguazt <marco.guazzone@gmail.com>
2012-10-12 09:16:40 +0200

    (bug-fix:minor) Forgot to add to repo.

---
+++ inc/dcs/cli.hpp
@@ -0,0 +1,40 @@
+/**
+ * \file dcs/cli.hpp
+ *
+ * \brief Functions for parsing command line options passed to programs.
+ *
+ * \author Marco Guazzone (marco.guazzone@gmail.com)
+ *
+ * <hr/>
+ *
+ * Copyright (C) 2012       Marco Guazzone
+ *                          [Distributed Computing System (DCS) Group,
+ *                           Computer Science Institute,
+ *                           Department of Science and Technological Innovation,
+ *                           University of Piemonte Orientale,
+ *                           Alessandria (Italy)]
+ *
+ * This file is part of dcsxx-commons.
+ *
+ * dcsxx-commons is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * dcsxx-commons is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with dcsxx-commons. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef DCS_CLI_HPP
+#define DCS_CLI_HPP
+
+
+#include <dcs/cli/simple.hpp>
+
+
+#endif // DCS_CLI_HPP
