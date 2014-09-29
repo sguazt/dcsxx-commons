283733c167637e4d9ff0347dd44664818346c695
sguazt <marco.guazzone@gmail.com>
2014-09-29 18:04:28 +0200

    [algorithm] Added variant of 'map_values' function taking a collection of keys for which we want to get their values from the map

--- inc/dcs/algorithm/map.hpp
+++ inc/dcs/algorithm/map.hpp
@@ -46,11 +46,58 @@ std::vector<T> map_values(MapIterT first, MapIterT last)
 	return res;
 }
 
-/// Returns the values of the map \c m
+/// Returns the values of the map defined by the [\a map_first, \a map_last) iterator pair
+template <typename T, typename MapIterT, typename KeyIterT>
+std::vector<T> map_values(MapIterT map_first, MapIterT map_last, KeyIterT key_first, KeyIterT key_last)
+{
+	std::vector<T> res;
+
+	MapIterT map_it = map_first;
+
+	while (map_first != map_last)
+	{
+		KeyIterT key_it = key_first;
+
+		while (key_it != key_last)
+		{
+			if (map_first->first == *key_it)
+			{
+				res.push_back(map_first->second);
+				break;
+			}
+
+			++key_it;
+		}
+
+		++map_first;
+	}
+
+	return res;
+}
+
+/// Returns the values of the map \a m
 template <typename MapT>
 std::vector<typename MapT::mapped_type> map_values(MapT const& m)
 {
 	return map_values<typename MapT::mapped_type>(m.begin(), m.end());
+}
+
+/// Returns the values of the map \a m corresponding to keys defined by the [\a key_first, \a key_last] iterator pair
+template <typename MapT, typename KeyIterT>
+std::vector<typename MapT::mapped_type> map_values(MapT const& m, KeyIterT key_first, KeyIterT key_last)
+{
+	std::vector<typename MapT::mapped_type> res;
+
+	while (key_first != key_last)
+	{
+		if (m.count(*key_first) > 0)
+		{
+			res.push_back(m.at(*key_first));
+		}
+
+		++key_first;
+	}
+	return res;
 }
 
 /// Returns the keys of the map defined by the [\a first, \a last) iterator pair
