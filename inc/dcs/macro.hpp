be89075c79338bd8b8702f7dab142c824e2e7d4b
sguazt <marco.guazzone@gmail.com>
2015-01-15 19:37:43 +0100

    [macro] (new) New macros for checking the availability of C++11 and C++14 standards. (change) Improved macros for marking deprecated declarations

--- inc/dcs/macro.hpp
+++ inc/dcs/macro.hpp
@@ -49,20 +49,48 @@ inline void suppress_unused_variable_warning(T const&) {}
 /// Stringifies (quote) the given argument.
 #define DCS_MACRO_QUOTE(x) #x
 
+/// Tells if we're compiling with a C++11 compatible compiler
+#if __cplusplus >= 201103L
+# define DCS_MACRO_CXX11
+#else
+# undef DCS_MACRO_CXX11
+#endif // DCS_MACRO_CXX111
+
+/// Tells if we're compiling with a C++14 compatible compiler
+#if __cplusplus >= 201300L
+# define DCS_MACRO_CXX14
+#else
+# undef DCS_MACRO_CXX14
+#endif // DCS_MACRO_CXX114
+
 /// Suppresses the "unused variable" warning issued during compilation.
 #define DCS_MACRO_SUPPRESS_UNUSED_VARIABLE_WARNING(x) \
 	::dcs::macro::detail::suppress_unused_variable_warning(x)
 
-/// Declares a function, a variable or a type declaration as deprecated. Add it at the end of the declaration you want to deprecate.
-#if defined(_WIN32) || defined(__HP_cc)
-# define DCS_MACRO_DECL_DEPRECATED __declspec(deprecated)
-#elif defined(__GNUC__) && defined(__linux__)
-# define DCS_MACRO_DECL_DEPRECATED __attribute__ ((deprecated))
-#elif defined(__SUNPRO_C)
-# define DCS_MACRO_DECL_DEPRECATED
+/// Declares a function, a variable or a type declaration x as deprecated, and
+/// provides a suitable message m.
+/// Usage:
+/// - DCS_MACRO_DECL_DEPRECATED(int v, "Variable v is deprecated");
+/// - DCS_MACRO_DECL_DEPRECATED(void f(int), "Function f is deprecated");
+/// - DCS_MACRO_DECL_DEPRECATED(void f(int) { }, "Function f is deprecated")
+/// - struct DCS_MACRO_DECL_DEPRECATED(S, "Struct S is deprecated") { };
+#ifdef DCS_MACRO_CXX14
+# define DCS_MACRO_DECL_DEPRECATED(x,m) [[deprecated(m)]] x
 #else
-# define DCS_MACRO_DECL_DEPRECATED
-#endif
+//# if defined(_WIN32) || defined(__HP_cc)
+# if defined(_MSC_VER)
+#  define DCS_MACRO_DECL_DEPRECATED(x,m) __declspec(deprecated) x
+//# elif defined(__GNUC__) && defined(__linux__)
+# elif defined(__GNUC__)
+#  define DCS_MACRO_DECL_DEPRECATED(x,m) x __attribute__ ((deprecated))
+//# elif defined(__SUNPRO_C)
+//#  define DCS_MACRO_DECL_DEPRECATED
+# else
+// Don't know how to make it. Sorry!
+// Add a warning comment.
+#  define DCS_MACRO_DECL_DEPRECATED(x,m) /*XXX: Deprecated declaration!!*/ x
+# endif
+#endif // DCS_MACRO_CXX14
 
 /// Marks a function, a variable or a type declaration that should be exported from DLLs or binaries for runtime linking.
 #if defined(_WIN32) || defined(__HP_cc)
